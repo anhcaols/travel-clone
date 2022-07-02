8d52e9d440e62fe5163988e61fe617d2893d5b7f
unknown <caotheanhls2001>
2022-07-02 18:13:46 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const sliderMain = $(".slider-main ")
+const barSliderItem = $$(".bar-slider-item")
+const imgSliderItems = $$(".slide-beaches-item")
+const openSideBarBtn = $('.header-menu__img')
+const closeSideBarBtn = $('.side-bar-package__close')
+const closeSideBarBtn1 = $('.side-bar-package__close-mbtl')
+const sidebar = $(".side-bar")
+const sidebarMbTl = $(".side-bar-tablet-mobile")
+
+var i = 0
+var arr = []
+// Xu ly slider
+function handelSlider(){
+        barSliderItem.forEach((elem, index) => {
+            const imgItem = imgSliderItems[index]
+            arr.push(
+                elem.onclick = function(){
+                    $(".bar-slider-item.active").classList.remove("active")
+                    elem.classList.add("active")
+                    $(".slide-beaches-item.active").classList.remove("active")
+                    imgItem.classList.add("active")
+                    i = index
+                }
+            )
+        })  
+        setInterval(function() {
+            arr[i]();
+            i++;
+            if(i > arr.length-1) i=0;
+        },5000) 
+}
+// Su ly dong/ mo side bar
+function handelSideBar(){
+    openSideBarBtn.addEventListener("click", () => {
+        sidebar.classList.add("show")
+        sidebar.style.animation = "RightToLeft linear 0.5s "
+
+        sidebarMbTl.classList.add("show")
+        sidebarMbTl.style.animation = "RightToLeft linear 0.5s "
+
+    })
+    closeSideBarBtn1.addEventListener("click", () => {
+        sidebar.classList.remove("show")
+        sidebar.style.animation = "LeftToRight linear 0.5s "
+
+        sidebarMbTl.classList.remove("show")
+        sidebarMbTl.style.animation = "LeftToRight linear 0.5s "
+    })
+}
+function start(){
+    handelSlider()
+    handelSideBar()
+}
+start()
